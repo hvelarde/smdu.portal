930c27c3beb88c477fd81445ca5db7935d0211cb
hvelarde <hector.velarde@gmail.com>
2016-08-25 14:49:53 -0300

    Activate theme by default

---
+++ src/smdu/portal/tests/test_theme.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from smdu.portal.testing import INTEGRATION_TESTING
+
+import unittest
+
+
+class ThemeTestCase(unittest.TestCase):
+
+    """Ensure theme is properly installed."""
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+
+    def test_available_themes(self):
+        from plone.app.theming.utils import getAvailableThemes
+        themes = getAvailableThemes()
+        self.assertEqual(len(themes), 3)
+
+    def test_theme_enabled(self):
+        from plone.app.theming.utils import isThemeEnabled
+        self.assertTrue(isThemeEnabled(self.request))
+
+    def test_current_theme(self):
+        from plone.app.theming.utils import getCurrentTheme
+        theme = getCurrentTheme()
+        self.assertEqual(theme, u'Default')
+
+    def test_default_theme(self):
+        from plone.app.theming.utils import getTheme
+        theme = getTheme('Default')
+        self.assertIsNotNone(theme)
+        self.assertEqual(theme.title, 'SMDU Portal')
+        self.assertEqual(theme.description, 'Tema para portal da SMDU')
+        self.assertEqual(theme.absolutePrefix, '/++theme++Default')
+        self.assertEqual(theme.rules, '/++theme++Default/rules.xml')
+        self.assertEqual(theme.doctype, '<!DOCTYPE html>')
